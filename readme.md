2a6779bc64366f1c502c97440ec94673cc36327a
EnmingZhang <zhangenming@gomeplus.com>
2017-02-28 16:05:25 +0800

    upd ripple related

--- readme.md
+++ readme.md
@@ -10,10 +10,15 @@
 5. 使用了 google 自家漂亮的 Roboto Mono 字体，标题的各词之间用`·`和`¬`点缀
 
 逐条实现：
-1. 给波纹元素一个固定宽高，相对于 header 绝对定位，初始时隐藏。按钮上触发 mousedown 时，取得事件相对于整个 header 的坐标，分别减掉波纹元素的宽高，设为波纹的位置。此时如果按住鼠标，则波纹扩散至给定的尺寸后不动，这里使用 keyframes 定义 from scale(0) to scale(1) 即可；这个过程中如果放开鼠标，则设置神奇的 animation-play-state 属性为 paused，再定义 keyframes 为 to scale(20) 后恢复 animation-play-state: running 就好了。因为未定义起始帧，该动画会自动从前一个动画结尾的 scale 程度计算。  
+1. 给波纹元素一个固定宽高，相对于 header 绝对定位，初始时隐藏。按钮上触发 mousedown 时，取得事件相对于整个 header 的坐标，分别减掉波纹元素的宽高，设为波纹的位置。此时如果按住鼠标，则波纹扩散至给定的尺寸后不动，这里使用 keyframes 定义 from scale(0) to scale(1) 即可；这个过程中如果放开鼠标，则设置神奇的 animation-play-state 属性为 paused，再定义 keyframes 为 to scale(18) 后恢复 animation-play-state: running 就好了。因为未定义起始帧，该动画会自动从前一个动画结尾的 scale 程度计算。  
 
-这里有个小坑，就是 mouseup 事件的目标必然会是波纹元素，不可能是任何别的。这导致我 debug 了半天，还怀疑 mouseup 事件是不是不冒泡，蠢死了。
+这里有个小坑，就是 mouseup 事件的目标必然会是波纹元素，不可能是任何别的。我 debug 了半天，还怀疑 mouseup 事件是不是不冒泡，蠢死了。
 
 这样一搞，mousedown -> mouseup -> click 这个过程有种明显的段落感，很舒服。Google Design 这里的处理似乎更复杂，是在新标签页渲染完毕之后触发 mouseup 的动画。
+
+2.
+
+4. header 的底部有个阴影。我先看了下 google 的做法，是在 header 元素后面添加一个 fake-shadow 元素，该元素的背景是横向重复的一张阴影 png。但这样做的话，header 元素必须被放进另一个 fixed 定位的容器元素内。不太明白为什么要这样？我用 box-shadow 实现了类似的，这个属性的兼容性还是不错的。阴影颜色选用了 element 提供的 light-silver(#99A9BF)。  
+然后给 window 的 scroll 事件添加一个监听，同时修改 layer 的高度即可。注意，前面的波纹元素需要修改一下，mouseup 时需要先渐变窗口的 scrollTop 至 0。
 
 <!-- $('#header').on('mousedown', '.nav-anchor', function(){}) -->
--- src/js_es6/index.js
+++ src/js_es6/index.js
@@ -1,31 +1,59 @@
-let $header = $('#gds-header'),
-    $ripple = $header.children('#ripple');
+let $window = $(window),
+    $body = $('body'),
+    $header = $('#gds-header'),
+    $ripple = $header.children('#ripple'),
+    $navButtons = $header.find('.nav-anchor'),
+    $rippleLayer = $header.find('.ripple-layer');
 
+let $navButtonClicked = null;
 
-$('#gds-header')
+$header
     .on('mousedown', '.nav-anchor', function(evt) {
         $ripple
             .css({
                 left: evt.pageX - 50,
-                top: evt.pageY - 50,
+                // top 值要减掉窗口的垂直滚动偏移
+                top: evt.pageY - 50 - document.body.scrollTop,
             })
             .addClass('noneToCircle');
+        $navButtonClicked = $(this);
     })
+    .on('click', '.nav-anchor', function(evt) {
+        $navButtons.removeClass('active');
+        $(this).addClass('active');
+    });
+
+$body
     .on('mouseup', function(evt) {
         // 根据事件目标的话，智能判断 mousedown，无法判断 mouseup，因为后者的目标永远是波纹元素。
         // 所以以波纹元素是否已有动画类为标准，决定如何处理
         if ($ripple.hasClass('noneToCircle')) {
-            $ripple
-                .css({
-                    'animation-play-state': 'paused',
-                })
-                .removeClass('noneToCircle')
-                .addClass('circleToFullscreen')
-                .css({
-                    'animation-play-state': 'running',
-                });
-            setTimeout(function() {
-                $ripple.removeClass('noneToCircle circleToFullscreen');
-            }, 400)
+            // 先将窗口的 scrollTop 渐变至 0
+            $body.animate({scrollTop: 0}, 200, function() {
+                $ripple
+                    .css({
+                        'animation-play-state': 'paused',
+                    })
+                    .removeClass('noneToCircle')
+                    .addClass('circleToFullscreen')
+                    .css({
+                        'animation-play-state': 'running',
+                    });
+                setTimeout(function() {
+                    // 移除波纹元素的动画类
+                    $ripple.removeClass('noneToCircle circleToFullscreen');
+                    //  如果 $navButtonClicked 不为 null，则在它上面触发 click 事件
+                    if ($navButtonClicked !== null) {
+                        $navButtonClicked.click();
+                    }
+                }, 300);
+            });
         }
-   });
+    });
+
+$window
+    .on('scroll', function(evt) {
+        let layerHeight = 192 - document.body.scrollTop;
+        (layerHeight < 0) && (layerHeight = 0);
+        $rippleLayer.height(layerHeight);
+    });
