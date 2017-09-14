ac108047ce9ebd4e018f198ecf0c67e8d19a4548
EnmingZhang <zhangenming@gomeplus.com>
2017-09-14 17:10:03 +0800

    upd

--- webpackfile.js
+++ webpackfile.js
@@ -2,7 +2,7 @@ const webpack = require('webpack')
 const path = require('path')
 const fs = require('fs')
 
-const INPUT_PATH = path.join(__dirname, '/_js')
+const INPUT_PATH = path.join(__dirname, '/_javascripts')
 const OUTPUT_PATH = path.join(__dirname, '/')
 
 // const extractSass = new ExtractTextPlugin({
@@ -12,39 +12,45 @@ const OUTPUT_PATH = path.join(__dirname, '/')
 
 module.exports = {
     watch: true,
-    entry: entries,
+    entry: path.resolve(__dirname, INPUT_PATH, 'jqueryMaterial'),
     output: {
         path: path.resolve(__dirname, OUTPUT_PATH),
-        filename: 'jQueryMaterial',
+        filename: 'jqueryMaterial',
     },
     module: {
-        rules: [{
-            test: /\.js$/,
-            exclude: /node_modules/,
-            use: {
-                loader: 'babel-loader',
-                options: {
-                    presets: ['es2015'],
+        rules: [
+            {
+                test: /\.js$/,
+                exclude: /node_modules/,
+                use: {
+                    loader: 'babel-loader',
+                    options: {
+                        presets: ['es2015'],
+                    },
                 },
             },
-        }, {
-            test: /\.scss$/,
-            use: [{
-                loader: "style-loader" // 将 JS 字符串生成为 style 节点
-            }, {
-                loader: "css-loader" // 将 CSS 转化成 CommonJS 模块
-            }, {
-                loader: "sass-loader" // 将 Sass 编译成 CSS
-            }, ],
-        }, {
-            // IDEA: webpack 字体打包
-            test: /\.(jpg|png|ttf)$/,
-            use: [
-                {
+            {
+                test: /\.scss$/,
+                use: [
+                    {
+                        loader: "style-loader" // 将 JS 字符串生成为 style 节点
+                    },
+                    {
+                        loader: "css-loader" // 将 CSS 转化成 CommonJS 模块
+                    },
+                    {
+                        loader: "sass-loader" // 将 Sass 编译成 CSS
+                    },
+                ],
+            },
+            {
+                // IDEA: webpack 字体打包
+                test: /\.(jpg|png|ttf)$/,
+                use: {
                     loader: 'url-loader',
                 },
-            ],
-        }],
+            },
+        ],
     },
     plugins: [
         // new webpack.optimize.UglifyJsPlugin({
