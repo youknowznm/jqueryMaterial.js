2ac9a4dd078beb5bba513063d6ffbac9c59e2f12
youknowznm <znm92@icloud.com>
2017-09-15 15:31:52 +0800

    header sass refact

---
+++ _old_javascripts/md-button.js
@@ -0,0 +1,90 @@
+import $ from './jquery.js'
+
+export default function initMdButton() {
+
+    function initButton($mdButton) {
+        $mdButton.data('animating', false)
+    }
+
+    $('.md-button, .md-icon-button').each(function() {
+        initButton($(this))
+    })
+
+    $('body')
+        .on('mousedown', '.md-button:not(._disabled)', function(evt) {
+            let $this = $(this)
+            if ($this.data('animating') === false) {
+                $this.data('clicked', true)
+                let $ripple = $this.find('.ripple')
+                let _x = evt.offsetX
+                let _y = evt.offsetY
+                let _width = $this.innerWidth()
+                let _height = $this.innerHeight()
+                // 根据事件坐标和按钮水平中点的距离，获取波纹的直径
+                let offsetToHorizontalCenter = _width / 2 - _x
+                let offsetToVerticalCenter = _height / 2 - _y
+                let sideLength = _width + Math.abs(offsetToHorizontalCenter) * 2 + (_width / 10)
+                $ripple.css({
+                    width: sideLength,
+                    height: sideLength,
+                    left: (_width - sideLength) / 2 - offsetToHorizontalCenter,
+                    top: (_height - sideLength) / 2 - offsetToVerticalCenter,
+                })
+                $this.addClass('mousedown')
+            }
+        })
+        .on('mouseup mouseout', '.md-button:not(._disabled)', function() {
+            let $this = $(this)
+            if ($this.data('animating') === false && $this.data('clicked') === true) {
+
+                // 设置timeout，避免mousedown事件持续时间过短导致的闪烁
+                setTimeout(function() {
+
+                    $this.data({
+                        animating: true,
+                        clicked: false
+                    })
+                    $this.removeClass('mousedown').addClass('mouseup')
+                    setTimeout(function() {
+                        $this.removeClass('mouseup')
+                        $this.data('animating', false)
+                    }, 550)
+
+                }, 250)
+
+            }
+        })
+
+    $('body')
+        .on('mousedown', '.md-icon-button:not(._disabled)', function(evt) {
+            let $this = $(this)
+            if ($this.data('animating') === false) {
+                $this.data('animating', true)
+                let $ripple = $this.find('.ripple')
+                $ripple.css({
+                    width: '40px',
+                    height: '40px',
+                    left: '20px',
+                    top: '20px',
+                })
+                $this.addClass('mousedown')
+                setTimeout(function() {
+                    $this.removeClass('mousedown').addClass('mouseup')
+                    setTimeout(function() {
+                        $this.removeClass('mouseup')
+                        $this.data('animating', false)
+                    }, 300)
+                }, 300)
+            }
+        })
+
+    // 浮动提示条
+    $('body')
+        .on('mouseenter', '.show-tooltip', function(evt) {
+            $(this).children('.md-tooltip').addClass('show')
+        })
+        .on('mouseleave', '.show-tooltip', function(evt) {
+            $(this).children('.md-tooltip').removeClass('show')
+        })
+
+}
---
+++ _old_javascripts/md-textarea.js
@@ -0,0 +1,69 @@
+import $ from './jquery.js'
+
+export default function initMdTextarea() {
+
+    function initTextarea($mdTextarea) {
+        let $textarea = $mdTextarea.children('textarea')
+        let val = $textarea.val()
+        // 若textarea内容非空，则添加non-empty类
+        $mdTextarea.toggleClass('non-empty', /\S/.test(val))
+        let currentCharCount = val.length
+        let maxCharCount = $textarea.attr('maxlength')
+        $mdTextarea.find('.current').text(currentCharCount)
+        $mdTextarea.find('.maximum').text(maxCharCount)
+        // ‘未点击’状态的标识。在输入框产生初次blur后修改
+        $mdTextarea.data('edited', false)
+        $textarea.one('blur', function() {
+            $mdTextarea.data('edited', true)
+        })
+    }
+
+    $('.md-textarea').each(function() {
+        initTextarea($(this))
+    })
+
+    $('.md-textarea ._input')
+        .on('focus', function() {
+            let $this = $(this)
+            let $wrap = $this.parent()
+            $wrap.addClass('focused')
+            $wrap.toggleClass('non-empty', $this.val() !== '')
+        })
+        .on('blur', function() {
+            let $this = $(this)
+            let $wrap = $this.parent()
+            $wrap.removeClass('focused')
+            $wrap.toggleClass('non-empty', $this.val() !== '')
+            validate(this)
+        })
+        .on('keyup', function() {
+            validate(this)
+        })
+
+    function validate(inputEle) {
+        let $this = $(inputEle)
+        let $wrap = $this.parent()
+        // 字数验证
+        let currentCount = $this.val().length
+        let currentCharCounter = $wrap.find('.current')
+        let maxCharCount = +$wrap.find('.maximum').text()
+        currentCharCounter.text(currentCount)
+        // $wrap.toggleClass('invalid', currentCount === maxCharCount);
+    }
+
+    setTimeout(function() {
+        let $flexibleInput = $('.md-textarea.responsive-height ._input')
+        if ($flexibleInput.length > 0) {
+            let response = function() {
+                $flexibleInput
+                    .height(1)
+                    .height($flexibleInput[0].scrollHeight)
+            }
+            response()
+            $flexibleInput.on('input', function() {
+                response()
+            })
+        }
+    }, 0)
+
+}
